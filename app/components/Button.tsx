736f2e7145af3752e76fef8366141e199b68d23f
manlikexavy <chibokaxavier@gmail.com>
2024-12-03 17:16:35 +0100

    done with button styling

---
+++ app/components/Button.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+interface ButttonProps {
+  type: "button" | "submit";
+  title: string;
+  variant: string;
+  icon: string;
+}
+
+const Button = ({ type, title, variant, icon }: ButttonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label>{title}</label>
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { NAV_LINKS } from "../constants";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <nav className=" flexBetween max-container padding-container relative z-30 py-5">
+      <Link href={"/"}>
+        <Image src={"/hilink-logo.svg"} alt="" width={74} height={29} />
+      </Link>{" "}
+      <ul className="hidden h-full  gap-12 lg:flex">
+        {NAV_LINKS.map((nav, index) => (
+          <Link
+            key={index}
+            href={`/${nav.href}`}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold "
+          >
+            {nav.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button type='button' title='Login' icon='/user.svg' variant='btn_dark_green' />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
