b824150cec3244f2357af7022219d15e6997506d
manlikexavy <chibokaxavier@gmail.com>
2024-12-04 14:33:36 +0100

    getApp section complete

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,14 +5,17 @@ interface ButttonProps {
   type: "button" | "submit";
   title: string;
   variant: string;
-  icon: string;
+  icon?: string;
+  full?: boolean;
 }
 
-const Button = ({ type, title, variant, icon }: ButttonProps) => {
+const Button = ({ type, title, variant, icon, full }: ButttonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      } `}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
